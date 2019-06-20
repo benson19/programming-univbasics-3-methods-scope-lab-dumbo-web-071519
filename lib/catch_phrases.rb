1f25f970457daaf8e9d64a59f192146bb72d492b
carlos matthew guzman <tomaffew@me.com>
2019-06-20 17:07:55 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -7,3 +7,8 @@ def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+def link
+  phrase1 = "It's Dangerous To Go Alone! Take This."
+  puts phrase1
+end
